62a2b498ca097c560c134306dfd3f3d9a270d824
David Taylor <dktaylor1701@gmail.com>
2019-02-24 19:37:29 -0500

    Updated sha

--- Formula/xcsnippets.rb
+++ Formula/xcsnippets.rb
@@ -4,7 +4,7 @@ class Xcsnippets < Formula
   desc "XCode Snippet Manager"
   homepage "https://github.com/dtaylor1701/XCSnippets"
   url "https://github.com/dtaylor1701/XCSnippets/archive/v1.0.0.tar.gz", version: "v1.0.0"
-  sha256 "e6b1994468667b491c99bb867f35e9fd0a42dda2159d9c333b2c47d314eb1c53"
+  sha256 "62665207ddf01d151e40fa80f94e424fcd112914b9eb1b99b129d0e2cdd18cf0"
   head "https://github.com/dtaylor1701/XCSnippets.git"
   
   depends_on :xcode => ["10.0", :build]

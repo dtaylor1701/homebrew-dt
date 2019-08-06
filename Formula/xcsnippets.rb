acd7d5238e6920a9da0bf1139029824aa2e065d4
David Taylor <dktaylor1701@gmail.com>
2019-08-05 23:10:34 -0400

    Update to xcsnippets

--- Formula/xcsnippets.rb
+++ Formula/xcsnippets.rb
@@ -1,13 +1,13 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://www.rubydoc.info/github/Homebrew/brew/master/Formula
 class Xcsnippets < Formula
-  desc "XCode Snippet Manager"
+  desc "Xcode Snippet Manager"
   homepage "https://github.com/dtaylor1701/XCSnippets"
-  url "https://github.com/dtaylor1701/XCSnippets/archive/v1.0.1.tar.gz", version: "v1.0.1"
+  url "https://github.com/dtaylor1701/XCSnippets/archive/v1.0.2.tar.gz", version: "v1.0.2"
   sha256 "75c5b5c5afd3a8ecad20fa10e644468577f5893f8ce006cba281d4c59617c90a"
   head "https://github.com/dtaylor1701/XCSnippets.git"
   
-  depends_on :xcode => ["10.0", :build]
+  depends_on :xcode => ["10.2", :build]
 
   def install
       system "make", "install", "prefix=#{prefix}"
